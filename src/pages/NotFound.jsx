a6c98d5036ef75b398a45503bd4e8a948f93fbb8
mukhammadziyo <mukhammadziyo@gmail.com>
2025-02-17 16:07:53 +0500

    ./

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,11 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function NotFound() {
   return (
     <div>
-      <img
-        className="w-full h-screen"
-        src="./public/404 not found.jpg"
-        alt=""
-      />
+      <img className="w-full h-screen" src="public/404 not found.jpg" alt="" />
       <NavLink
         to={"/"}
         className="absolute top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 bg-gray-500 text-white px-4 py-2 rounded"
